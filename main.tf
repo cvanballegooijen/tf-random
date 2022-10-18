5054be92e3ded980d6eeb7ea8d0359d341050e4a
Cojan van Ballegooijen <40221816+cvanballegooijen@users.noreply.github.com>
2022-10-18 08:39:41 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,8 @@
 # for an AWS EC2 instance that changes each time a new AMI id is
 # selected.
 
-resource "random_pet" "server" {
-  keepers = {
-    # Generate a new pet name each time we switch to a new AMI id
-    ami_id = var.ami_id
-  }
+resource "random_pet" "pet1" {
+
 }
 
 

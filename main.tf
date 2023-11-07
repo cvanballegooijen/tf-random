d92a753ad530e7cabad10b8f384dec8860d828d1
Cojan van Ballegooijen <40221816+cvanballegooijen@users.noreply.github.com>
2023-11-07 14:45:33 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-f# The following example shows how to generate a unique pet name
+# The following example shows how to generate a unique pet name
 # for an AWS EC2 instance that changes each time a new AMI id is
 # selected.
 

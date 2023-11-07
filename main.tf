c67c4cc47f60f4e3f894d5616d1544775c35991e
Cojan van Ballegooijen <40221816+cvanballegooijen@users.noreply.github.com>
2023-11-07 14:42:54 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-# The following example shows how to generate a unique pet name
+f# The following example shows how to generate a unique pet name
 # for an AWS EC2 instance that changes each time a new AMI id is
 # selected.
 
 resource "random_pet" "pet1" {
   keepers = {
     # Generate a new pet name each time we switch to a new AMI id
-    pet_name = var.pet_name
+    pet_default_name = var.pet_default_name
   }
 
 }
